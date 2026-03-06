b07e51ddaa9e36e5cfe8265c010c81303d37e34b
sabrinamarim <sabrinamarimdac@gmail.com>
2026-03-06 08:51:14 -0300

    Update desafio_1004.py

--- desafio_1004.py
+++ desafio_1004.py
@@ -15,3 +15,9 @@ caso contrário seu programa apresentará a mensagem: "Presentation Error".
 # Link do problema: https://judge.beecrowd.com/pt/problems/view/1004
 
 # Escreva sua solução abaixo
+A = int(input())
+B = int(input())
+
+PROD = A * B
+
+print(f"PROD = {PROD}")
