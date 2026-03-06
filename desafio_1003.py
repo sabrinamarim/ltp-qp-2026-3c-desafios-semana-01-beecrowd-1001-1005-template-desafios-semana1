4966e4fe2bcd534b21b3e98f72e563c85ed5ddd1
sabrinamarim <sabrinamarimdac@gmail.com>
2026-03-06 08:43:18 -0300

    Update desafio_1003.py

--- desafio_1003.py
+++ desafio_1003.py
@@ -15,3 +15,9 @@ de A e B.
 # Link do problema: https://judge.beecrowd.com/pt/problems/view/1003
 
 # Escreva sua solução abaixo
+A = int(input())
+B = int(input())
+
+SOMA = A + B
+
+print(f"SOMA = {SOMA}")
