31252e70e3d0a8979ae879291ba47b7c55cec92f
sabrinamarim <sabrinamarimdac@gmail.com>
2026-03-06 08:53:21 -0300

    Update desafio_1005.py

--- desafio_1005.py
+++ desafio_1005.py
@@ -18,3 +18,9 @@ de imprimir o fim de linha após o resultado, caso contrário, você receberá
 # Link do problema: https://judge.beecrowd.com/pt/problems/view/1005
 
 # Escreva sua solução abaixo
+A = float(input())
+B = float(input())
+
+MEDIA = (A * 3.5 + B * 7.5) / 11
+
+print(f"MEDIA = {MEDIA:.1f}")
